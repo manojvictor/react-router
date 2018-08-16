90524c1e5220ca798025b646b630397c62b9f2d5
manojvictor <fmanojvictor@gmail.com>
2018-08-16 15:52:23 +0530

    rect router basic

---
+++ app/components/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = React.createClass({
+  render: function() {
+    return (
+      <div className="home-page">
+        <b>React Router - Basic</b>
+        <p>
+        React has been designed from the start for gradual adoption, and you can use as little or as much React as you need. Whether you want to get a taste of React, add some interactivity to a simple HTML page, or start a complex React-powered app, the links in this section will help you get started.
+        </p>
+        <p>
+        When starting a React project, a simple HTML page with script tags might still be the best option. It only takes a minute to set up!
+        </p>
+      </div>
+    );
+  }
+});
+
+export default Home;
